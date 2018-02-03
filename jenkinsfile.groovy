95e3a59391984adbaa15b04f85e3db5b0f55eae5
ofir gutmacher <ofirgut007@gmail.com>
2018-02-03 12:57:33 +0200

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,33 +1,33 @@
-tools { 
-        maven 'Maven 3.3.9' 
-        jdk 'jdk8' 
-    }
-
-	properties([
-        disableConcurrentBuilds(),
-        parameters([
-                string(defaultValue: '', description: '', name: 'my_branch'),
-        ]),
-        pipelineTriggers([])
-])
-
 node("master") {
-	stage('Check-out') {
-            checkout([$class: 'GitSCM', branches: [[name: "master"]], doGenerateSubmoduleConfigurations: true, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "/home/ec2-user/WORKSPACE/"]], submoduleCfg: [], userRemoteConfigs: [[url: "git remote add origin https://github.com/ofirgut007/spring-boot-examples"]]])	
-	}
-
-    stage('Build') {
-        try {
-            sh 'mvn -Dmaven.test.failure.ignore=true install' 
-	} catch (exc) {
-	    error "ERROR: Failed to checkout branch - ${params.my_branch}"
-	}
-    }
-	stage ('test') {
-			junit 'target/surefire-reports/**/*.xml' 
-	}
-    stage('Deploy') {
-        
-
-    }
+   withMaven(maven:'m1') {
+      stage('Checkout') {
+         checkout([$class: 'GitSCM', branches: [[name: "ofir"]], doGenerateSubmoduleConfigurations: true, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "/home/ec2-user/WORKSPACE/"]], submoduleCfg: [], userRemoteConfigs: [[url: "git remote add origin https://github.com/ofirgut007/spring-boot-examples"]]])	
+      }
+      stage('Build') {
+         try {
+            sh 'mvn clean test package'
+	    def pom = readMavenPom file:'pom.xml'
+            print pom.version
+            env.version = pom.version
+	 } catch (exc) {
+	    error "ERROR: Failed to build maven"
+	 }
+      }
+      stage ('test') {
+         junit 'spring-boot-package-war/target/surefire-reports/TEST-*.xml' 
+      }
+      stage('Docker') {
+         dir ('discovery-service') {
+            def dockerimage = docker.build "localhost:5000/discovery-service:${env.version}"
+            dockerimage.push()
+            }
+         }
+      }
+      stage ('Run') {
+         docker.image("localhost:5000/discovery-service:${env.version}").run('-p 8761:8761 -h discovery --name discovery')
+      }
+      stage ('Final') {
+         build job: 'account-service-pipeline', wait: false
+      }      
+   }
 }
