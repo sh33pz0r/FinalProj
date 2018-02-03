89cd1c3c4e83cb81296286da483ac6ba71cbf0b2
ofir gutmacher <ofirgut007@gmail.com>
2018-02-03 13:53:36 +0200

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,30 +4,54 @@ node("master") {
          checkout([$class: 'GitSCM', branches: [[name: "ofir"]], doGenerateSubmoduleConfigurations: true, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "/home/ec2-user/WORKSPACE/"]], submoduleCfg: [], userRemoteConfigs: [[url: "git remote add origin https://github.com/ofirgut007/spring-boot-examples"]]])	
       }
       stage('Build') {
+         dir('spring-boot-package-war'){
          try {
-            sh 'mvn clean test package'
+            sh 'mvn clean package -DskipTests'
 	    def pom = readMavenPom file:'pom.xml'
             print pom.version
             env.version = pom.version
+	    def workspacePath = pwd()
+	    sh "echo ${env.version} > ${workspacePath}/envversion.txt"
+	    mvn versions:set versions:commit -DnewVersion="0.0.25"
+	    mvn scm:checkin -Dincludes=pom.xml -Dmessage="Setting version, preping for release."
 	 } catch (exc) {
-	    error "ERROR: Failed to build maven"
+	    error "ERROR: Failed to package maven"
+	 }
 	 }
       }
       stage ('test') {
-         junit 'spring-boot-package-war/target/surefire-reports/TEST-*.xml' 
-      }
-      stage('Docker') {
+	 dir ('spring-boot-package-war') {
+            sh "mvn test"
+	    junit 'spring-boot-package-war/target/surefire-reports/TEST-*.xml' 
+	 }
+      }	
+      stage('Deploy') {
          dir ('spring-boot-package-war') {
-            def dockerimage = docker.build "localhost:5000/spring-boot-package-war:${env.version}"
+            def dockerimage = docker.build "spring-boot-package-war:${env.version}"
+            //def dockerimage = docker.build "spring-boot-package-war:${env.BUILD_NUMBER}"
             dockerimage.push()
-            }
-         }
+          }
       }
       stage ('Run') {
-         docker.image("localhost:5000/spring-boot-package-war:${env.version}").run('-p 8761:8761 -h discovery --name discovery')
+	 try {
+            docker.image("spring-boot-package-war:${env.version}").run('-p 8761:8761')
+         } catch (error) {
+	 } finally {
+	 }
       }
+
       stage ('Final') {
          //build job: 'do-stuff-with-container-pipeline', wait: false
-      }      
+      }
+      def deploymentOk(){
+         def workspacePath = pwd()
+         expectedCommitid = new File("${workspacePath}/envversion.txt").text.trim()
+         //actualCommitid = readCommitidFromJson()
+         println "expected version from txt: ${expectedCommitid}"
+         //println "actual version from json: ${actualCommitid}"
+         //return expectedCommitid == actualCommitid
+	 return 1
+}
+
    }
 }
